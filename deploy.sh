d3c4fcdacbd614160611872fa00de10c7fc38999
Xavier <xavier@netbattletech.com>
2017-07-11 19:46:06 -0700

    update deploy script to include per-branch tags and repo push

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 DO_LOGIN=0
+RESTART=0
+TASK_DEFINITION=""
+CLUSTER=""
 
-while getopts "l" opt; do
+while getopts "lr" opt; do
     case "$opt" in
-    l) DO_LOGIN=1
-       ;;
+    l) DO_LOGIN=1 ;;
+    r) RESTART=1 ;;
     esac
 done
 
@@ -16,10 +19,20 @@ BRANCH=`git rev-parse --abbrev-ref HEAD`
 TAG=""
 
 case $BRANCH in
-    "master") TAG="dev" ;;
-    "production") TAG="prod" ;;
+    "master") 
+        TAG="DEV"  
+        CLUSTER="default" 
+        ;;
+
+    "production") 
+        TAG="PROD" 
+        CLUSTER="NBT-PROD" 
+        ;;
+
     *) echo "On non-deployment branch $BRANCH, exiting"; exit 0 ;;
 esac
+
+TASK_DEFINITION="NBT-API-$TAG"
 
 # login, if requested
 if (( DO_LOGIN > 0 )); then
@@ -32,3 +45,17 @@ docker build -t nbt-web:$TAG .
 # deploy to AWS ECR
 docker tag nbt-web:$TAG 068936280303.dkr.ecr.us-west-2.amazonaws.com/nbt-web:$TAG
 docker push 068936280303.dkr.ecr.us-west-2.amazonaws.com/nbt-web:$TAG
+
+# restart the tasks if requested
+if (( RESTART > 0 )); then
+    TASK=`aws ecs list-tasks --cluster=$CLUSTER | jq -rM '.["taskArns"][0]'`
+
+    if [ "$TASK" != "null" ]
+    then
+        aws ecs stop-task --task $TASK
+        sleep 5
+    fi
+
+    aws ecs run-task --task-definition $TASK_DEFINITION
+fi
+
