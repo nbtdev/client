33ccd56fed98b70762c8b4a099528a9c18f0f4b0
Xavier <xavier@netbattletech.com>
2017-07-18 00:02:55 -0700

    point to public dev API

--- live/js/nbt/nbt.js
+++ live/js/nbt/nbt.js
@@ -39,8 +39,8 @@ var Locale = {
 // root API url
 //var API_URL = 'http://api.home.lan';
 //var API_URL = 'http://tsunami:8080';
-//var API_URL = 'https://api-dev.netbattletech.com';
-var API_URL = 'http://api-home.netbattletech.com';
+var API_URL = 'https://api-dev.netbattletech.com';
+//var API_URL = 'http://api-home.netbattletech.com';
 
 
 function NBT() {
